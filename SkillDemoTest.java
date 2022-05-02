7510a26e4c6629c62be92d986bf35155b4379c91
Andrewdi02 <and002@ucsd.edu>
2022-05-01 23:45:57 -0700

    added test files

---
+++ SkillDemoTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SkillDemoTest {
+    @Test
+    public void multiTest(){
+        assertEquals(1, SkillDemo.multi(1, 2));
+    }
+}
